b2ec38927a0637970024318c4720ec981fd862b7
jonbro <jonbro@gmail.com>
2011-07-24 15:20:17 +0100

    tweaked the submission a bit so that it always does the ajax rather than form submission

--- public/js/search_system.js
+++ public/js/search_system.js
@@ -1,12 +1,16 @@
 // defines the workings of the search system
-$("#submit_title_search").click(function(){
-	// go get the results from the server
-	console.log("test");
-	var searchValue = $('#title_input').val();
-	$.getJSON('/search/'+searchValue, parseSearchResults)
+$("#submit_title_search").click(getSearchResults);
+$("#search_box").submit(function(){
+	getSearchResults();
 	return false;
 });
 
+var getSearchResults = function(){
+	// go get the results from the server
+	var searchValue = $('#title_input').val();
+	$.getJSON('/search/'+searchValue, parseSearchResults)
+	return false;
+}
 
 var parseSearchResults = function(results){
 	console.log(results);
@@ -14,9 +18,17 @@ var parseSearchResults = function(results){
 	$("#search_results").html("");
 	$.each(results, function(i,v){
 		// build the html
+		var web = "";
+		if(v.website){
+			web = "<a href='"+v.website+"'>web &raquo;</a>";
+		}
+		var last = false;
+		if(i==results.length-1){
+			last = true;
+		}
 		var eventHtml = [
-				"<div class='event festival_"+v.festival_id+"' id='event_"+v.code+"'>",
-				"<div class='name'>"+v.title+"</div>",
+			"<div class='event festival_"+v.festival_id+(last?" event_last":"")+"' id='event_"+v.code+"'>",
+				"<div class='name'>"+v.title+" "+web+"</div>",
 				"<div class='venue'><span class='bold'>@</span>"+v.venue.name+"</div>",
 			"</div>"
 		].join("\n");
