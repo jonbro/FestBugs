dbf675bde4b478cfc749d116ce2971c9c6b4c9c2
jonbro <jonbro@gmail.com>
2011-07-25 15:11:56 +0100

    saving and loading the data stored within a pond

--- festbugs.rb
+++ festbugs.rb
@@ -4,6 +4,19 @@ require 'base64'
 require 'cgi'
 require 'openssl'
 require 'net/http'
+require 'sequel'
+require 'json'
+
+DB = Sequel.connect(ENV['DATABASE_URL'] || "amalgalite://blog.db")
+
+# data models!
+
+class Pond < Sequel::Model
+	many_to_many :events
+end
+class Event < Sequel::Model
+	many_to_many :ponds
+end
 
 def urlSafeBase64Decode(base64String)
   return Base64.decode64(base64String.tr('-_','+/'))
@@ -29,4 +42,31 @@ get '/search/:name' do
 	}
 	resp_text = resp.body
 	return resp_text
+end
+
+get '/pond/create/:events' do
+	# generate a new pond
+	ponds = DB[:ponds]
+	@pond = Pond.new()
+	@pond.save()
+	p params[:events].split(" ")
+	# for each of the events that were chucked at us
+	params[:events].split(" ").each{|event|
+		# find or create the associated event in the db
+		@event = Event.find_or_create(:url => event)
+		if not @pond.events.include?(@event)
+			@pond.add_event(@event)
+		end
+	}
+	return @pond[:id].to_s
+end
+
+get '/pond/:id' do
+	# return the ids for all the events in the pond as a json array
+	@pond = Pond.find(:id => params[:id])
+	event_ids = []
+	@pond.events.each{|event|
+		event_ids.push(event[:url].to_s)
+	}
+	return event_ids.to_json
 end
